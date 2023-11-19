3934038044a6cbbcdb3f7a71a87f07178f3c4006
Typicek <carotkacz333@seznam.cz>
2023-11-19 15:30:54 +0100

    GDT

--- boot/boot.asm
+++ boot/boot.asm
@@ -21,10 +21,25 @@ global _start:function (_start.end - _start)
 _start:
     mov esp, stack_top
 
-    extern kernel_main
-    call kernel_main
+        extern kernel_main
+        call kernel_main
 
         cli
 .hang:  hlt
         jmp .hang
 .end:
+
+        extern gp
+        global gdt_flush
+
+        gdt_flush:
+            lgdt[gp]
+            mov ax, 0x10
+            mov ds, ax
+            mov es, ax
+            mov fs, ax
+            mov gs, ax
+            mov ss, ax
+            jmp 0x08:flush2
+        flush2:
+            ret
