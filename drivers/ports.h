80694ec0b38e92575232246e17388e1358775996
Typicek <carotkacz333@seznam.cz>
2023-11-19 20:34:09 +0100

    ports delete, vga update

--- drivers/ports.h
+++
@@ -1,7 +0,0 @@
-#ifndef _KERNEL_PORTS_H
-#define _KERNEL_PORTS_H
-
-unsigned char port_byte_in(unsigned short port);
-void port_byte_out(unsigned short port, unsigned char data);
-
-#endif // !_KERNEL_PORTS_H
